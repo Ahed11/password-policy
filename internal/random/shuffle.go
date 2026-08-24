488bdb8f637c918b0581327a77762b3edd91148e
Ahed11 <ahdghandi73@gmail.com>
2026-08-24 22:04:31 +0300

    перемешивание Фишера - Йетса

---
+++ internal/random/shuffle.go
@@ -0,0 +1,32 @@
+package random
+
+import "fmt"
+
+func Shuffle(source Source, n int, swap func(i, j int)) error {
+	if n < 0 {
+		return fmt.Errorf("shuffle size must be greater than or equal to 0")
+	}
+
+	if n <= 1 {
+		return nil
+	}
+
+	if source == nil {
+		return fmt.Errorf("random source is nil")
+	}
+
+	if swap == nil {
+		return fmt.Errorf("shuffle swap function is nil")
+	}
+
+	for i := n - 1; i > 0; i-- {
+		j, err := Range(source, i+1)
+		if err != nil {
+			return fmt.Errorf("shuffle index for position %d: %w", i, err)
+		}
+
+		swap(i, j)
+	}
+
+	return nil
+}
