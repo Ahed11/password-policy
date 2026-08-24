982995a241a7284806c3b1788908abaffc950c8c
Ahed11 <ahdghandi73@gmail.com>
2026-08-24 22:02:18 +0300

    добавил публичный API построения алфавитов

---
+++ internal/alphabet/api.go
@@ -0,0 +1,66 @@
+package alphabet
+
+import "sort"
+
+type ClassDefinition struct {
+	Name     string
+	Alphabet string
+}
+
+type Class struct {
+	Name     string
+	Alphabet []rune
+}
+
+type BuildResult struct {
+	Classes []Class
+	Union   []rune
+}
+
+func Build(definitions []ClassDefinition, exclude string) (BuildResult, []error) {
+	internalDefinitions := make([]classDefinition, 0, len(definitions))
+
+	for _, definition := range definitions {
+		internalDefinitions = append(
+			internalDefinitions,
+			classDefinition{
+				name:     definition.Name,
+				alphabet: definition.Alphabet,
+			},
+		)
+	}
+
+	normalizedClasses, diagnostics := buildClasses(internalDefinitions, exclude)
+
+	result := BuildResult{
+		Classes: make([]Class, 0, len(normalizedClasses)),
+	}
+
+	unionSet := make(map[rune]struct{})
+
+	for _, normalized := range normalizedClasses {
+		result.Classes = append(
+			result.Classes,
+			Class{
+				Name:     normalized.name,
+				Alphabet: normalized.alphabet,
+			},
+		)
+
+		for _, r := range normalized.alphabet {
+			unionSet[r] = struct{}{}
+		}
+	}
+
+	result.Union = make([]rune, 0, len(unionSet))
+
+	for r := range unionSet {
+		result.Union = append(result.Union, r)
+	}
+
+	sort.Slice(result.Union, func(i, j int) bool {
+		return result.Union[i] < result.Union[j]
+	})
+
+	return result, diagnostics
+}
