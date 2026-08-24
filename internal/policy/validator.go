4e34cd12ecde6769b110a089725ad5804dc957ef
Ahed11 <ahdghandi73@gmail.com>
2026-08-24 15:08:24 +0300

    проверка repeat_run

--- internal/policy/validator.go
+++ internal/policy/validator.go
@@ -108,5 +108,9 @@ func ValidatePolicy(config Config) []error {
 		errors = append(errors, fmt.Errorf("issue.rotate_after: %w", rotateAfterErr))
 	}
 
+	if config.Policy.Forbid.RepeatRun < 0 {
+		errors = append(errors, fmt.Errorf("policy.forbid.repeat_run: must be greater than or equal to 0"))
+	}
+
 	return errors
 }
